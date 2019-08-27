f9e89f949ecf3b676d8ca1f187cd26f61e2f96c2
Your Name <you@example.com>
2019-08-27 13:58:24 -0700

    start server add protestors

--- server.js
+++ server.js
@@ -7,7 +7,17 @@ const config = require("./config.json");
 
 const PORT = config.port;
 
+// Define middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Displays all the protestors
 app.get("/api/protesters", function (req, res) {
-    
+    return res.json(protesters);
 });
 
+// Start the server
+// ==============================================
+app.listen(PORT, function() {
+    console.log(`API Server now listening on PORT ${PORT}!`);
+})
